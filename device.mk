307fda1c5fb427e7b7bfc52bde10a77a4cf5188f
Roger Ortiz <rogerortizleal@gmail.com>
2020-02-11 09:00:18 +0100

    TB3-850F: Working TWRP tree.
    Signed-off-by: Roger Ortiz <rogerortizleal@gmail.com>

---
+++ device.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := device/Lenovo/TB3_850F
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/prebuilt/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/rootdir,root) 
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/verity.mk)
+
+ifneq (,$(filter eng,$(TARGET_BUILD_VARIANT)))
+	PRODUCT_SUPPORTS_BOOT_SIGNER := true
+	PRODUCT_VERITY_SIGNING_KEY := build/target/product/security/verity
+endif
+
+PRODUCT_NAME := TB3_850F
---
+++ omni_TB3_850F.mk
@@ -0,0 +1,10 @@
+# Inherit device configuration
+$(call inherit-product, device/Lenovo/TB3_850F/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := TB3_850F
+PRODUCT_NAME := omni_TB3_850F
+PRODUCT_BRAND := Lenovo
+PRODUCT_MANUFACTURER := Lenovo
+PRODUCT_MODEL := Lenovo Tab TB3-850F
+
